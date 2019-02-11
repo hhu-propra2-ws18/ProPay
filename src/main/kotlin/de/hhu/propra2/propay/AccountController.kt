df5269bad7fc9d6104cc8238a1b0128950fd6f85
Christian Meter <christian.meter@uni-duesseldorf.de>
2019-02-11 17:22:55 +0100

    Add CORS Headers to controller

--- src/main/kotlin/de/hhu/propra2/propay/AccountController.kt
+++ src/main/kotlin/de/hhu/propra2/propay/AccountController.kt
@@ -11,17 +11,20 @@ class AccountController {
     @Autowired
     lateinit var moneyService: MoneyService
 
+    @CrossOrigin(origins = ["*"])
     @GetMapping("/account/{account}")
     @ApiOperation(value = "Zeigt Accountinformationen an", notes = "Gibt die Informationen zu einem Account zurück. Wenn der Account noch nicht existierte, wird ein leerer Account angelegt.")
     fun getAccount(@PathVariable account: String): Account =
             moneyService.getAccount(account)
 
+    @CrossOrigin(origins = ["*"])
     @PostMapping("/account/{account}")
     @ApiOperation(value = "Erhöhe / Verringere das Guthaben eines Accounts", notes = "Veränderung des Accountguthabens. Ein Account kann kein negatives Guthaben haben.")
     fun postDeposit(@PathVariable account: String,
                     @RequestParam amount: Double): Account =
             moneyService.deposit(account, amount)
 
+    @CrossOrigin(origins = ["*"])
     @PostMapping("/account/{sourceAccount}/transfer/{targetAccount}")
     @ApiOperation(value = "Überweisung tätigen", notes = "Überweise Betrag vom `sourceAccount` auf auf `targetAccount`.")
     fun postTransfer(@PathVariable sourceAccount: String,
--- src/main/kotlin/de/hhu/propra2/propay/ReservationController.kt
+++ src/main/kotlin/de/hhu/propra2/propay/ReservationController.kt
@@ -3,10 +3,7 @@ package de.hhu.propra2.propay
 import de.hhu.propra2.propay.services.ReservationService
 import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class ReservationController {
@@ -14,6 +11,7 @@ class ReservationController {
     @Autowired
     lateinit var reservationService: ReservationService
 
+    @CrossOrigin(origins = ["*"])
     @PostMapping("/reservation/reserve/{account}/{targetAccount}")
     @ApiOperation(value = "Reservierungen erzeugen", notes = "Reserviert einen vorgegebenen Betrag auf dem `account`. Benötigt einen Zielaccount, zu dem der reservierte Betrag bei Bedarf hin übertragen wird.")
     fun postReserve(@PathVariable account: String,
@@ -21,12 +19,14 @@ class ReservationController {
                     @RequestParam amount: Double): Reservation =
             reservationService.reserve(account, targetAccount, amount)
 
+    @CrossOrigin(origins = ["*"])
     @PostMapping("/reservation/release/{account}")
     @ApiOperation(value = "Reservierung aufheben", notes = "Löse eine Reservierung. In diesem Fall wird dem Account sein Guthaben wieder zur freien Verfügung gegeben und die Reservierung wird restlos gelöscht.")
     fun postRelease(@PathVariable account: String,
                     @RequestParam reservationId: Long): Account =
             reservationService.releaseReservation(account, reservationId)
 
+    @CrossOrigin(origins = ["*"])
     @PostMapping("/reservation/punish/{account}")
     @ApiOperation(value = "Reservierung aufheben und Account bestrafen", notes = "Zieht dem Account das reservierte Guthaben ab und überträgt den reservierten Betrag an den zuvor definierten `targetAccount`.")
     fun postPunish(@PathVariable account: String,
