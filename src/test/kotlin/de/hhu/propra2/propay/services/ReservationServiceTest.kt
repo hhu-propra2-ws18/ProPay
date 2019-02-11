ed1e430ebd79379f019edc7dd72a12989a2c448d
David Schneider <david.schneider@bivab.de>
2019-02-11 15:38:34 +0100

    Use file based h2 database

---
+++ src/test/kotlin/de/hhu/propra2/propay/services/ReservationServiceTest.kt
@@ -0,0 +1,20 @@
+package de.hhu.propra2.propay.services
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ReservationServiceTest {
+
+    @Test
+    fun reserve() {
+    }
+
+    @Test
+    fun releaseReservation() {
+    }
+
+    @Test
+    fun punishAndTransferReservation() {
+    }
+}
